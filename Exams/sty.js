818cfc018280c5755602119fe78fef629cd00c4c
Banoth Rajesh <120710438+Chinna532@users.noreply.github.com>
2024-03-02 23:22:53 +0530

    Add files via upload

---
+++ Exams/sty.js
@@ -0,0 +1,42 @@
+function rajesh(){
+    console.log("rajeshnayak")
+}
+rajesh()
+
+function y(a,b){
+    console.log(a+b)
+  }
+ y(2,4)
+
+
+
+function x(a,b){
+  return a*b
+}
+console.log(x(2,4))
+
+
+z=function(){
+    console.log("skillset academy")
+}
+z()
+
+x=()=>{console.log("hello")}
+x()
+
+x=()=>console.log("hi")
+x()
+
+x=(a,c)=>{console.log("hii,hello")}
+x()
+
+x=_=>{console.log("Banoth")}
+x()
+
+
+function c(){
+    let c=function y(){
+    return "hello"
+    }
+    y()
+    }
